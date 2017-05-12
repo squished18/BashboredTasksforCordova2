7c8010a57c50016b74186105c30c9f7e3230c50a
David Lau <squished18@gmail.com>
2017-05-11 18:38:00 -0700

    dlau

--- www/js/database_tasks.js
+++ www/js/database_tasks.js
@@ -6,6 +6,8 @@ function database_tasks_update_version_1()
 
 function update_database()
 {
+    console.log("update_database called");
+    console.log(localStorage['database_tasks_version']);
     if (!localStorage['database_tasks_version'])
     {
         database_tasks_update_version_1();
